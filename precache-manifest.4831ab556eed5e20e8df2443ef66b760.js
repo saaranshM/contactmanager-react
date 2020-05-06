1b45c11f27025c7dca1674e6a8db185616d05781
Saaransh Menon <saaransh.menon2000@gmail.com>
2020-05-07 01:16:34 +0530

    Updates

---
+++ precache-manifest.4831ab556eed5e20e8df2443ef66b760.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7830051a28c7d90930e47f6258a6e45",
+    "url": "/contactmanager-react/index.html"
+  },
+  {
+    "revision": "31e00857ea8416b5aff6",
+    "url": "/contactmanager-react/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "46cfa29dd0de1f941431",
+    "url": "/contactmanager-react/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "31e00857ea8416b5aff6",
+    "url": "/contactmanager-react/static/js/2.b8ada158.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager-react/static/js/2.b8ada158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46cfa29dd0de1f941431",
+    "url": "/contactmanager-react/static/js/main.792f0e99.chunk.js"
+  },
+  {
+    "revision": "b7e48d61aaef2315601d",
+    "url": "/contactmanager-react/static/js/runtime-main.6f9a476c.js"
+  }
+]);
